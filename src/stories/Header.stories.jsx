643fbc16995cd59b1160f4dc4b3ecae259ac379b
shixuewen <friendlysxw@163.com>
2022-03-25 18:45:55 +0800

    fix: update script

--- src/stories/Header.stories.jsx
+++ src/stories/Header.stories.jsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-25 17:06:48
+ * @LastEditTime: 2022-03-25 18:15:10
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \storybook-app\src\stories\Header.stories.jsx
+ */
 import React from 'react';
 
 import { Header } from './Header';
@@ -7,7 +15,7 @@ export default {
   component: Header,
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout:'centered',
   },
 };
 
