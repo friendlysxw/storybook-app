65a3000c66a9e63a498b00cccb21a7e7dd631312
shixuewen <friendlysxw@163.com>
2022-03-25 19:06:52 +0800

    fix: update author

--- src/stories/Header.stories.jsx
+++ src/stories/Header.stories.jsx
@@ -1,8 +1,8 @@
 /*
- * @Author: your name
+ * @Author: shixuewen
  * @Date: 2022-03-25 17:06:48
- * @LastEditTime: 2022-03-25 18:15:10
- * @LastEditors: your name
+ * @LastEditTime: 2022-03-25 19:05:56
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \storybook-app\src\stories\Header.stories.jsx
  */
